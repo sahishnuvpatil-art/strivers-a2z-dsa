3b1d7d49baefbea7e36e3e435ee9bd3dd250d479
sahishnu patil <sahishnuvpatil@gmail.com>
2026-08-11 19:24:35 +0530

    Print GFG n times

---
+++ basic-recursion/PrintGFGntimes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG {
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+       
+        for(int i=0;i<n;i++){
+            System.out.print("GFG ");
+        }
+    }
+}
